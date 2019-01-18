c107ff92c23c394b650766d76fbd40914468fdc5
Tom Elliott <tomelliottnz@gmail.com>
2019-01-18 13:10:21 +1300

    Add data scripts

---
+++ data/fetch_data.R
@@ -0,0 +1,16 @@
+# fetch historical data from raspberry pi
+date <- "2019-01-15" # tuesday, jan 15 2019
+pi <- Sys.getenv("pi_ip")
+
+hfile <-
+    file.path(
+        '/mnt', 'storage', 'history',
+        format(as.Date(date), '%Y/%m/%d'),
+        paste0(
+            'archive_',
+            format(as.Date(date), '%Y_%m_%d'),
+            '.zip'
+        )
+    )
+
+system(sprintf('scp tom@%s:%s archive.zip', pi, hfile))
