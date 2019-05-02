47ed8ae600515a6199ac25adb81b18fdd43ed81e
Tom Elliott <tomelliottnz@gmail.com>
2019-05-02 21:28:50 +1200

    add script

---
+++ chapters/chapter02/load_gtfs.R
@@ -0,0 +1,26 @@
+library(RSQLite)
+url <- "https://cdn01.at.govt.nz/data/gtfs.zip"
+db <- "at_gtfs.sqlite"
+outdated <- function(url) {
+    date <- gsub("Last-Modified: |\r", "", system(sprintf("curl -sI %s | grep Last-Modified", url), intern = TRUE))
+    last.mod <- as.POSIXct(date, tz = "GMT", format = "%a, %d %b %Y %H:%M:%S GMT")
+
+    cur.mod <- ".gtfs_date"
+    if (!file.exists(cur.mod)) {
+        writeLines(as.character(last.mod), cur.mod)
+        return(TRUE)
+    }
+    prev.mod <- as.POSIXct(readLines(cur.mod), tz = "GMT")
+    if (prev.mod < last.mod) {
+        writeLines(as.character(last.mod), cur.mod)
+        return(TRUE)
+    }
+    FALSE
+}
+atzip <- "at_gtfs.zip"
+if (!file.exists(atzip))
+    download.file(url, atzip)
+if (!file.exists(db) || outdated(url)) {
+    if (file.exists(db)) unlink(db)
+    nw <- transitr::create_gtfs(atzip, db = db)
+}
