272d5c6ea8bbd4b5f7121e4e5f1bce392b9b7254
Tom Elliott <tomelliottnz@gmail.com>
2019-09-05 17:44:54 +1200

    KNN-reg for forecast + 2nd week data

--- chapters/chapter04/load_week_data.R
+++ chapters/chapter04/load_week_data.R
@@ -14,7 +14,7 @@ if (!file.exists("data_week0.rda")) {
             return(tt_all)
         d <- tools::file_path_sans_ext(basename(zip_archive))
         unzip(zip_archive, exdir = d)
-        on.exit(unlink(d))
+        on.exit(unlink(d, recursive = TRUE, force = TRUE))
         return(data_from_file(paste0(d, ".rda"), con, d))
     }) %>% bind_rows %>%
         mutate(
@@ -23,12 +23,39 @@ if (!file.exists("data_week0.rda")) {
             dow = format(timestamp, "%A") %>%
                 factor(levels = c("Monday", "Tuesday", "Wednesday", "Thursday",
                                   "Friday", "Saturday", "Sunday")),
-            time = as.POSIXct(format(timestamp, "%H:%M:%S"), format = "%H:%M:%S")
+            time = as.POSIXct(paste("2019-08-12", format(timestamp, "%H:%M:%S")))
         )
     dbDisconnect(con)
 
     save("data_week0", file = "data_week0.rda")
 } else {
     load("data_week0.rda")
+}
+
+
+if (!file.exists("data_week1.rda")) {
+    dates_week1 <- list.files("../../data", "archive_.+.zip", full.names = TRUE)[8:14]
+    con <- dbConnect(SQLite(), db)
+    data_week1 <- lapply(dates_week1, function(zip_archive) {
+        if (grepl("2019-08-20", zip_archive))
+            return(tt_future)
+        d <- tools::file_path_sans_ext(basename(zip_archive))
+        unzip(zip_archive, exdir = d)
+        on.exit(unlink(d, recursive = TRUE, force = TRUE))
+        return(data_from_file(paste0(d, ".rda"), con, d))
+    }) %>% bind_rows %>%
+        mutate(
+            timestamp = arrival_time,
+            date = format(timestamp, "%Y-%m-%d"),
+            dow = format(timestamp, "%A") %>%
+                factor(levels = c("Monday", "Tuesday", "Wednesday", "Thursday",
+                                  "Friday", "Saturday", "Sunday")),
+            time = as.POSIXct(paste("2019-08-12", format(timestamp, "%H:%M:%S")))
+        )
+    dbDisconnect(con)
+
+    save("data_week1", file = "data_week1.rda")
+} else {
+    load("data_week1.rda")
 }
 
--- chapters/chapter04/week_model.stan
+++ chapters/chapter04/week_model.stan
@@ -6,24 +6,26 @@ data {
   int<lower=1,upper=7> d[N];      // day of the week (1=monday, ..., 7=sunday)
 }
 transformed data {
+  real center = mean(b);
+  vector[N] b_center = b - center;
   int<lower=0,upper=1> is_weekday[N];
   for (n in 1:N) {
     is_weekday[n] = d[n] < 6;
   }
 }
 parameters {
-  real<lower=0> mu;
+  real<lower=-center> mu;
 
   vector<lower=0,upper=1>[2] pi;
 
   vector<lower=6,upper=20>[2] mu_tau;
-  vector<lower=0,upper=2>[2] sigma_tau;
+  real<lower=0,upper=2> sigma_tau;
   matrix<lower=6,upper=20>[2,M] tau;
 
   vector<lower=0.1,upper=3>[2] omega;
 
   vector<lower=50,upper=10000>[2] mu_rho;
-  vector<lower=0,upper=1000>[2] sigma_rho;
+  real <lower=0,upper=1000> sigma_rho;
   matrix<lower=0,upper=10000>[2,M] rho;
 
   real<lower=0> err;
@@ -46,12 +48,13 @@ transformed parameters {
           eta += is_weekday[n] * rho[2,d[n]] *
               exp(-pow(t[n] - tau[2,d[n]], 2) * 0.5 * pow(omega[2], -2));
         }
-        lp[p1 + 2 * p2 + 1] += normal_lpdf(b[n] | eta, err);
+        lp[p1 + 2 * p2 + 1] += normal_lpdf(b_center[n] | eta, err);
       }
     }
   }
 }
 model {
+  mu ~ normal(0, 100);
   pi ~ beta(0.001, 0.001);
 
   mu_tau[1] ~ uniform(6, 10);
@@ -63,8 +66,8 @@ model {
 
   for (j in 1:2) {
     for (m in 1:M) {
-      tau[j,m] ~ normal(mu_tau[j], sigma_tau[j]);
-      rho[j,m] ~ normal(mu_rho[j], sigma_rho[j]);
+      tau[j,m] ~ normal(mu_tau[j], sigma_tau);
+      rho[j,m] ~ normal(mu_rho[j], sigma_rho);
     }
   }
 
