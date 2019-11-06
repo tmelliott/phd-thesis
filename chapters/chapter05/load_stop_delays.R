0b972358162d694edf83b4fc82b053dc25500457
Tom Elliott <tomelliottnz@gmail.com>
2019-11-06 13:27:53 +1300

    Truncate IDs to get better sample sizes

--- chapters/chapter05/load_stop_delays.R
+++ chapters/chapter05/load_stop_delays.R
@@ -53,6 +53,47 @@ if (!file.exists(stop_delays_file)) {
     load(stop_delays_file)
 }
 
+# dwell times
+if (file.exists("dwell_times.rda")) {
+    load("dwell_times.rda")
+} else {
+    dwell_times <- stop_delays %>%
+        group_by(trip_id, vehicle_id, stop_id, type) %>%
+        summarize(
+            delay = min(delay)
+        ) %>%
+        ungroup() %>%
+        spread(
+            key = type, value = delay
+        ) %>%
+        mutate(dwell = departure - arrival) %>%
+        # between 0 and 5 min
+        filter(between(dwell, 3, 5*60)) %>%
+        mutate(stop_id = stringr::str_replace(stop_id, "-.+", "")) %>%
+        group_by(stop_id) %>%
+        summarize(
+            avg = mean(dwell),
+            sd = sd(dwell),
+            n = n(),
+            se = sd / sqrt(n),
+            min = min(dwell),
+            max = max(dwell),
+            q5 = quantile(dwell, 0.05),
+            q25 = quantile(dwell, 0.25),
+            q50 = quantile(dwell, 0.5),
+            q75 = quantile(dwell, 0.75),
+            q95 = quantile(dwell, 0.95)
+        )
+
+    save(dwell_times, file = "dwell_times.rda")
+}
+
+library(RSQLite)
+con <- dbConnect(SQLite(), "../../../transitr/at_gtfs.db")
+dbWriteTable(con, "dwell_times", dwell_times, overwrite = TRUE)
+dbDisconnect(con)
+
+
 ## clean stop delays (by day/trip)
 if (file.exists("delays.rda")) {
     load("delays.rda")
@@ -92,36 +133,103 @@ if (file.exists("delays.rda")) {
 #     geom_hex()
 
 ## explorateur
-smry <- delays %>%
-    filter(between(delay, -30*60, 2*60*60)) %>%
+#
+# merge route ID
+library(RSQLite)
+con <- dbConnect(SQLite(), "../../../transitr/at_gtfs.db")
+trip <- dbGetQuery(con, "SELECT route_id, trip_id FROM trips")
+dbDisconnect(con)
+
+
+delays2 <- delays %>%
+    left_join(trip, by = "trip_id") %>%
+    ungroup() %>%
+    mutate(
+        stop_id = stringr::str_replace(stop_id, "-.+", ""),
+        trip_id = stringr::str_replace(trip_id, "-.+", ""),
+        route_id = stringr::str_replace(route_id, "-.+", "")
+    ) %>%
+    filter(between(delay, -30*60, 2*60*60))
+
+smry <- delays2 %>%
     group_by(trip_id, stop_id) %>%
     summarize(
         stop_sequence = first(stop_sequence),
-        avg_delay = mean(delay),
-        sd_delay = sqrt(var(delay)),
-        n = n()
+        route_id = first(route_id),
+        avg = mean(delay),
+        sd = sd(delay),
+        n = n(),
+        se = sd / sqrt(n),
+        min = min(delay),
+        max = max(delay),
+        q5 = quantile(delay, 0.05),
+        q25 = quantile(delay, 0.25),
+        q50 = quantile(delay, 0.5),
+        q75 = quantile(delay, 0.75),
+        q95 = quantile(delay, 0.95)
     )
 
-smry2 <- delays %>%
+smry2 <- delays2 %>%
+    filter(between(delay, -30*60, 2*60*60)) %>%
+    group_by(route_id, stop_id) %>%
+    summarize(
+        stop_sequence = first(stop_sequence),
+        avg = mean(delay),
+        sd = sd(delay),
+        n = n(),
+        se = sd / sqrt(n),
+        min = min(delay),
+        max = max(delay),
+        q5 = quantile(delay, 0.05),
+        q25 = quantile(delay, 0.25),
+        q50 = quantile(delay, 0.5),
+        q75 = quantile(delay, 0.75),
+        q95 = quantile(delay, 0.95)
+    )
+
+smry3 <- delays2 %>%
     filter(between(delay, -30*60, 2*60*60)) %>%
     group_by(stop_sequence) %>%
     summarize(
-        avg_delay = mean(delay),
-        sd_delay = sqrt(var(delay)),
-        n = n()
+        avg = mean(delay),
+        sd = sd(delay),
+        n = n(),
+        se = sd / sqrt(n),
+        q50 = quantile(delay, 0.5)
     )
 
-smry <- smry %>%
+smry2 <- smry2 %>% 
     left_join(
-        smry2 %>% select(stop_sequence, sd_delay) %>% rename(err = sd_delay),
+        smry3 %>% select(stop_sequence, avg, sd, n, se, q50) %>% 
+            rename(avg2 = avg, sd2 = sd, n2 = n, se2 = se, q502 = q50),
         by = "stop_sequence"
     ) %>%
     mutate(
-        sd_delay = ifelse(is.na(sd_delay), err, sd_delay)
+        avg = ifelse(n < 10, avg2, avg),
+        sd = ifelse(n < 10, sd2, sd),
+        se = ifelse(n < 10, se2, se),
+        q50 = ifelse(n < 10, q502, q50),
+        n = ifelse(n < 10, n2, n)
     ) %>%
-    select(-err)
+    select(-avg2, -sd2, -n2, -se2, -q502)
+
+final_smry <- smry %>% ungroup() %>%
+    left_join(
+        smry2 %>% ungroup() %>% select(route_id, stop_sequence, avg, sd, n, se, q50) %>% 
+            rename(avg2 = avg, sd2 = sd, n2 = n, se2 = se, q502 = q50),
+        by = c("route_id", "stop_sequence")
+    ) %>%
+    mutate(
+        avg = ifelse(n < 10, avg2, avg),
+        sd = ifelse(n < 10, sd2, sd),
+        se = ifelse(n < 10, se2, se),
+        q50 = ifelse(n < 10, q502, q50),
+        n = ifelse(n < 10, n2, n)
+    ) %>%
+    select(-avg2, -sd2, -n2, -se2, -q502)
+
 
 library(RSQLite)
-con <- dbConnect(SQLite(), "../../../WIP/at_gtfs.db")
-dbWriteTable(con, "stop_delays", smry, overwrite = TRUE)
+con <- dbConnect(SQLite(), "../../../transitr/at_gtfs.db")
+dbWriteTable(con, "stop_delays", final_smry, overwrite = TRUE)
 dbDisconnect(con)
